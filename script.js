9df75e9de2309201854f8eb5d35fa5af68af4456
Benjamin Pham <benjaminpham@Benjamins-MacBook-Air.local>
2023-06-03 14:44:51 -0700

    Finish adding content from API

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-import { CLIENT_ID, CLIENT_SECRET, REDIRECT_URI } from "./key.js";
+import { CLIENT_ID, REDIRECT_URI } from "./key.js";
 
 const redirect = REDIRECT_URI;
 const client_id = CLIENT_ID;
-const client_secret = CLIENT_SECRET;
 
 const playlistsURL = "https://api.spotify.com/v1/me/playlists?offset=0&limit=20";
+const podcastsURL = "https://api.spotify.com/v1/me/shows?offset=0&limit=4";
+const episodesURL = "https://api.spotify.com/v1/me/episodes?offset=0&limit=4";
 
 const params = new URLSearchParams(window.location.search);
 const code = params.get("code");
@@ -13,9 +14,11 @@ if (!code) {
     redirectToAuthCodeFlow(client_id, redirect);
 } else {
     const accessToken = await getAccessToken(client_id, redirect, code);
-    const playlists = await fetchPlaylists(accessToken);
-    console.log(playlists.items);
-    populateUI(playlists.items);
+    const playlists = await fetchAPI(accessToken, playlistsURL);
+    const podcasts = await fetchAPI(accessToken, podcastsURL);
+    const episodes = await fetchAPI(accessToken, episodesURL);
+    console.log(episodes);
+    populateUI(playlists.items, podcasts.items, episodes.items);
 }
 
 async function redirectToAuthCodeFlow(clientId, redirectUri) {
@@ -29,7 +32,7 @@ async function redirectToAuthCodeFlow(clientId, redirectUri) {
     params.append("client_id", clientId);
     params.append("response_type", "code");
     params.append("redirect_uri", redirectUri);
-    params.append("scope", "user-read-private user-read-email playlist-read-private");
+    params.append("scope", "user-read-private user-read-email playlist-read-private user-library-read user-read-playback-position");
     params.append("code_challenge_method", "S256");
     params.append("code_challenge", challenge);
 
@@ -76,22 +79,22 @@ async function getAccessToken(clientId, redirectUri, code) {
     return access_token;
 }
 
-async function fetchPlaylists(token) {
+async function fetchAPI(token, url) {
     // TODO: Call Web API
-    const result = await fetch(playlistsURL, {
+    const result = await fetch(url, {
         method: "GET", headers: { Authorization: `Bearer ${token}` }
     });
 
     return await result.json();
 }
 
-function populateUI(playlists) {
-    // TODO: Update UI with profile data
+function populateUI(playlists, podcasts, episodes) {
+    // TODO: Update UI with data
     const playlist = playlists.map(p => {
         const list = document.createElement('a');
         list.classList.add('flex');
         list.innerHTML = `
-            <img src="assets/likedsongs.jpg" alt="">
+            <img src=${p.images[0] ? p.images[0].url : "assets/likedsongs.jpg"} alt="">
             <h4>${p.name}</h4>
         `;
         return list;
@@ -100,4 +103,61 @@ function populateUI(playlists) {
     playlist.forEach(p => {
         document.querySelector("aside .list").append(p);
     });
+
+    const podcast = podcasts.map(p => {
+        const list = document.createElement('div');
+        list.innerHTML = `
+            <img src=${p.show.images[0].url} alt="">
+            <h4>${p.show.name}</h4>
+            <h5>${p.show.publisher}</h5>
+        `;
+        return list;
+    });
+    
+    podcast.forEach(p => {
+        document.querySelector(".your-shows .list").append(p);
+    });
+
+    const episode = episodes.map(p => {
+        const list = document.createElement('div');
+        list.innerHTML = `
+            <img src=${p.episode.images[0].url} alt="">
+            <h4>${p.episode.name}</h4>
+            <h5>${p.episode.show.name}</h5>
+        `;
+        return list;
+    });
+    
+    episode.forEach(p => {
+        document.querySelector(".saved-episodes .list").append(p);
+    });
+
+    const list = `
+        <a href="#" class="flex">
+            <img src="assets/likedsongs.jpg" alt="">
+            <h3>Liked Songs</h3>
+        </a>
+        <a href="#" class="flex">
+            <img src=${playlists[0].images[0].url} alt="">
+            <h3>${playlists[0].name}</h3>
+        </a>
+        <a href="#" class="flex">
+            <img src=${playlists[1].images[0].url} alt="">
+            <h3>${playlists[1].name}</h3>
+        </a>
+        <a href="#" class="flex">
+            <img src="assets/thejournal.jpeg" alt="">
+            <h3>The Journal.</h3>
+        </a>
+        <a href="#" class="flex">
+            <img src="assets/effwon.jpeg" alt="">
+            <h3>eff won with DRS</h3>
+        </a>
+        <a href="#" class="flex">
+            <img src="assets/thepotterverse.png" alt="">
+            <h3>The Potterverse: A Harry Potter Podcast</h3>
+        </a>
+    `
+
+    document.querySelector(".playlists").innerHTML = list;
 }
